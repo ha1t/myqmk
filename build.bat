477e6e970ded505d5f818224108cdcfed60db907
halt <ha1t@users.noreply.github.com>
2023-03-16 14:39:13 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 set current_dir=%~dp0
 set target=crkbd/rev1/common:via
 
-docker run --rm -it -v %current_dir%qmk/qmk_firmware:/qmk_firmware qmkfm/base_container /bin/bash -c "cd /qmk_firmware && make %target%"
+docker run --rm -it -w /qmk_firmware -v %current_dir%qmk/qmk_firmware:/qmk_firmware qmkfm/qmk_cli make -j8 %target%
 
 pause
